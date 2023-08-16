f02635636f9edaed38c1cb78ff2c0afd03b26fbc
azalia-virna <115863304+azalia-virna@users.noreply.github.com>
2023-08-16 23:51:40 +0700

    Add files via upload

---
+++ database/migrations/2022_12_09_092250_create_wisatas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wisatas', function (Blueprint $table) {
+            $table->id();
+            $table->char('nama');
+            $table->char('kategori');
+            $table->char('deskripsi');
+            $table->char('file_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wisatas');
+    }
+};
